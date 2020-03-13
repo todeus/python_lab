dda0a398e23b9b2252a38043ce053550bc790a4e
todeus <gtodeus@gmail.com>
2020-03-13 14:44:04 +1000

    test commit from pycharm

--- task_6_2b.py
+++ task_6_2b.py
@@ -4,7 +4,10 @@ while True:
     oct = ip_addr.split(".")
     bool = True
     for oc in oct:
-        bool &= (int(oc) >= 0) & (int(oc) <= 255)
+        try:
+           bool &= (int(oc) >= 0) & (int(oc) <= 255)
+        except ValueError:
+            print("IP адрес должен быть в виде 192.168.1.100")
     if ((len(oct) == 4) & bool):
         probe = int(oct[0])
         if probe > 0:
