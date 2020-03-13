f4e6037f1a3e1c2e23d233fe6c00b94523da7868
todeus <gtodeus@gmail.com>
2020-03-13 15:10:01 +1000

    Lesson 7.1

--- task_7_1.py
+++ task_7_1.py
@@ -11,17 +11,17 @@ Outbound Interface: {interface}
 """
 
 for line in f:
-    protocol = line.split()[0].replace("O","OSPF").replace("B","BGP").replace("D","EIGRP")
+    protocol = line.split()[0].replace("O", "OSPF").replace("B", "BGP").replace("D", "EIGRP")
     prefix = line.split()[1]
     metric = line.split()[2].strip("[]")
     nexthop = line.split()[4].rstrip(",")
     lastupdate = line.split()[5].rstrip(",")
     interface = line.split()[6]
-    print(template.format(protocol = protocol,
-                          prefix = prefix,
-                          metric = metric,
-                          nexthop = nexthop,
-                          lastupdate = lastupdate,
-                          interface = interface))
+    print(template.format(protocol=protocol,
+                          prefix=prefix,
+                          metric=metric,
+                          nexthop=nexthop,
+                          lastupdate=lastupdate,
+                          interface=interface))
 
 f.close()
